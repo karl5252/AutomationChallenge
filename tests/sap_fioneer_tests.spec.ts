4bd05177f83fab209f68de210035c7854fdefeeb
karl5252 <karl.michalik@gmail.com>
2025-06-19 14:55:42 +0200

    Refactor test structure to improve readability and organization

--- tests/sap_fioneer_tests.spec.ts
+++ tests/sap_fioneer_tests.spec.ts
@@ -6,3 +6,11 @@ test('verify Key Facts section exists', async ({ page }) => {
   // TODO: Consider moving to a page object model for better maintainability once it grows
   await expect(page.locator('.row .col h2').filter({ hasText: 'Key Facts' })).toBeVisible();
 });
+
+test ('verify user is redirected to KSG engine when clicking on "KSG Engine" link', async ({ page }) => {
+  await page.goto('https://www.sapfioneer.com/', { waitUntil: 'networkidle' });
+
+  const ksgEngineBookmark = page.locator('#menu-item-29979').getByRole('link', { name: 'Finance & ESG' });
+  await expect(ksgEngineBookmark).toBeVisible();
+
+});
