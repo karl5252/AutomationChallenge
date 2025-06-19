8eb6f5e8dbfbb2be3e1b49c755ce62a5da452e54
karl5252 <karl.michalik@gmail.com>
2025-06-19 15:11:29 +0200

    Fix test description and update locator for KSG engine link verification

--- tests/sap_fioneer_tests.spec.ts
+++ tests/sap_fioneer_tests.spec.ts
@@ -7,10 +7,10 @@ test('verify Key Facts section exists', async ({ page }) => {
   await expect(page.locator('.row .col h2').filter({ hasText: 'Key Facts' })).toBeVisible();
 });
 
-test ('verify user is redirected to KSG engine when clicking on "KSG Engine" link', async ({ page }) => {
+test ('verify user is redirected to KSG engine when clicking on "ESG Engine" link', async ({ page }) => {
   await page.goto('https://www.sapfioneer.com/', { waitUntil: 'networkidle' });
 
-  const ksgEngineBookmark = page.locator('#menu-item-29979').getByRole('link', { name: 'Finance & ESG' });
+  const ksgEngineBookmark = page.locator('#masthead').getByRole('link', { name: 'Finance & ESG' });
   await expect(ksgEngineBookmark).toBeVisible();
 
   await ksgEngineBookmark.hover();
@@ -18,6 +18,6 @@ test ('verify user is redirected to KSG engine when clicking on "KSG Engine" lin
   await expect(ksgEngineLink).toBeVisible();
 
   await ksgEngineLink.click();
-
+  await expect(page).toHaveTitle('Stay audit-ready with the ESG KPI Engine | SAP Fioneer');
 
 });
